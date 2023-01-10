c4423c86a6ca6ae6d695d62a505f0f28f07531e5
dzaki1971 <dzaki1971@gmail.com>
2023-01-10 15:36:32 +0700

    f

---
+++ help.js
@@ -0,0 +1,19 @@
+
+exports.infoOwner = () =>{
+return`──「 *DATA PROFIL OWNER* 」──
+
+ ⬣ *Nama :* dzaki 
+ ⬣ *Umur :* Privv
+ ⬣ *Hoby. :* Turu
+ ⬣ *Asal   :* privv
+ ⬣ *Status:* Creator
+ ⬣ *Zodiak:* Gk tau
+
+                  「 *SOSIAL MEDIA*」
+ ⬣ *Whatsapp:* 085268084496
+ ⬣ *Youtube:* --
+ ⬣ *Github:* ---
+ ⬣ *Tiktok:* ---
+ ⬣ *Instagram:* ---
+ `
+}
